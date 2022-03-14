e1f962ca345cd6123a6ecbb7b34ebe2122189a7a
Augusto <jesus_augga_99@icloud.com>
2022-03-14 17:08:57 -0500

    add Add book and Remove book functions

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
 const collection = [
     {
-        title: 'loreum ipsum', 
+        title: '1', 
         author: 'Teasteroo Testyy'
     }, 
     {
-        title: 'loreum ipsum', 
+        title: '2', 
         author: 'Teasteroo Testyy'
     },
 ]
 
+function addBook(book) {
+  const cardHTML = `
+  <div class="book-container">
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <button class="remove-button">Remove</button>
+    <hr/>
+  </div>
+  `;
+  books.insertAdjacentHTML("beforeend", cardHTML);
+}
+
 const books = document.getElementById('books');
 collection.forEach((book) => {
- const cardHTML = `<div>
- <p>${book.title}</p>
- <p>${book.author}</p>
- <button class="remove-button">Remove</button>
- <hr />
-</div>`;
-books.insertAdjacentHTML("beforeend", cardHTML);
+ addBook(book);
 })
 
 
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+const titleForm = document.getElementById('title');
+const authorForm = document.getElementById('author');
 const form = document.getElementById('add-book');
+const submitButton = document.getElementById('submit');
+const removeButtons = document.querySelectorAll('.remove-button');
+const bookContainer = document.querySelectorAll('.book-container');
 
-form.addEventListener('submit', (event)=>{
-    collection.push({title: title.innerHTML, author: author.innerHTML
-    });
+submitButton.addEventListener('click', () => {
+    const book = {title: titleForm.value, author: authorForm.value};
+    collection.push(book);
+    addBook(book);
     console.log(collection);
 });
 
-
-
+removeButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    collection.splice(index, 1)
+    books.removeChild(bookContainer[index])
+    console.log(collection)
+  })
+})
